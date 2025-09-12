549fe7c4abdc164b03be55eaff915d0d79589e52
alvarocar86@gmail.com <alvarocar86example.com>
2025-09-12 09:24:39 -0500

    CLASE DEL 12 SEPT DE 2025

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import streamlit as st
+import plotly.express as px
+
 
 
 url = "https://github.com/alvarocar86/TABLERO-DE-CONTROL/raw/refs/heads/main/datos_generales_ficticios.csv"
@@ -36,14 +38,50 @@ st.dataframe(df)
 st.write(f"### Municipio con mas delitos: {max_municipio} con {max_cantidad_municipio} reportes")
 st.dataframe(df)
 
+etapa_mas_frecuente = df["ETAPA"].value_counts().index[0]
+cant_etapa_mas_frecuente = df["ETAPA"].value_counts().iloc[0]
+st.dataframe(df)
+
+st.set_page_config(page_title="Dashboard de Delitos - Fiscalia", layout="centered")
+st.title("Dashboard de Delitos - Fiscalia")
+st.dataframe(df)
+
+st.write(f"## Municipio con más delitos: {max_municipio} con {max_cantidad_municipio} reportes")
+st.write(f"{etapa_mas_frecuente} tiene {cant_etapa_mas_frecuente} registros")
+st.dataframe(df)
+
+st.subheader(f"## Municipio con más delitos: {max_municipio} con {max_cantidad_municipio} reportes")
+st.subheader(f"{etapa_mas_frecuente} tiene {cant_etapa_mas_frecuente} registros")
+st.dataframe(df)
+
+delitos = df["DELITO"].value_counts()
+st.write(delitos)
+
+st.subheader("Comportamiento Delitos")
+delitos = df["DELITO"].value_counts()
+st.bar_chart(delitos)
+
+st.subheader("Departamentos con mas casos")
+departamento = df["DEPARTAMENTO"].value_counts()
+st.bar_chart(departamento)
 
 
+st.subheader("Distribucion por Departamentos")
+fig =px.pie(
+    names=departamento.index,
+    values=departamento.values    
+)
+st.dataframe(df)
 
 
+fig.update_traces(textposition="outside", textinfo="percent+label")
+fig.update_layout(showlegend=False, height=400)
+st.plotly_chart(fig)
 
-
-
-
+df_delitos = df.groupby(['DEPARTAMENTO', 'DELITO']).size().reset_index(name='conteo')
+fig = px.bar(df_delitos, x='DEPARTAMENTO', y='conteo', color='DELITO', barmode='stack')
+st.plotly_chart(fig)
+st.write(df_delitos)
 
 
 
